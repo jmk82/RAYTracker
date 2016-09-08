8acdad71915761ccc583e8cb137610972eb3359a
Juha-Mikko Koskela <juhamikko@gmail.com>
2016-09-08 04:51:12 +0300

    change raytracker -> raytracker.ui

--- RAYTracker/RAYTracker/Properties/Resources.Designer.cs
+++ RAYTracker/RAYTracker/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RAYTracker.Properties {
+namespace RAYTracker.UI.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace RAYTracker.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RAYTracker.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("RAYTracker.UI.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- RAYTracker/RAYTracker/Properties/Settings.Designer.cs
+++ RAYTracker/RAYTracker/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace RAYTracker.Properties {
+namespace RAYTracker.UI.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
